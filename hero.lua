7ecb2512271428960ac42c5c719e98c5c41d6235
Wil Chung <iamwil@gmail.com>
2014-02-08 17:45:25 -0800

    hero can accelerate

--- hero.lua
+++ hero.lua
@@ -4,15 +4,21 @@ require('bullet')
 Hero = class:new()
 
 function Hero:init(x, y)
-  self.x = x
-  self.y = y
   self.width = 30
   self.height = 30
-  self.speed = 150
-end
 
-function Hero:bottom()
-  return self.y + self.height / 2
+  self.x = x
+  self.y = y
+
+  self.v_max = 150
+  self.vx = 0
+  self.vy = 0
+
+  self.a_max = 500
+  self.ax = 0
+  self.ay = 0
+
+  self.intent = nil
 end
 
 function Hero:draw()
@@ -26,6 +32,11 @@ end
 -- hero update actions
 
 function Hero:update(dt)
+  self:think(dt)
+  self:move(dt)
+end
+
+function Hero:think(dt)
   if love.keyboard.isDown("left") then
     hero:moveLeft(dt)
   end
@@ -40,20 +51,37 @@ function Hero:update(dt)
   end
 end
 
+function Hero:move(dt)
+  self.vx = self.vx + self.ax * dt
+  if self.vx > self.v_max then
+    self.vx = self.v_max
+  end
+  if self.vx < -self.v_max then
+    self.vx = -self.v_max
+  end
+
+  --self.vy = self.vy + self.ay * dt
+
+  self.x = self.x + self.vx * dt
+  --self.y = self.y + self.vx * dt
+end
+
+-- hero specific actions
+
 function Hero:moveLeft(dt)
-  self.x = self.x - self.speed * dt
+  self.ax = -self.a_max
 end
 
 function Hero:moveRight(dt)
-  self.x = self.x + self.speed * dt
+  self.ax = self.a_max
 end
 
 function Hero:moveUp(dt)
-  self.y = self.y - self.speed * dt
+  self.ay = -self.a_max
 end
 
 function Hero:moveDown(dt)
-  self.y = self.y + self.speed * dt
+  self.ay = self.a_max
 end
 
 function Hero:shoot(dt)
@@ -62,5 +90,9 @@ function Hero:shoot(dt)
   table.insert(world, shot)
 end
 
+
+function Hero:bottom()
+  return self.y + self.height / 2
+end
 
 
