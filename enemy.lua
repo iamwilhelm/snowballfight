a6888c97c7fcb34d503ae9280914fceebf0a739f
Wil Chung <iamwil@gmail.com>
2014-02-16 21:11:48 -0800

    can stun enemy

--- enemy.lua
+++ enemy.lua
@@ -12,13 +12,20 @@ function Enemy:init(x, y)
   if self ~= Enemy then
     self:setPosition(x, y)
     self:setDimension(32, 40)
-    self:setMoveForce(3000)
+    self:setMoveForce(2000)
     self:setMass(10)
+
+    self.stunTimestamp = love.timer.getTime()
+    self.state = "normal"
   end
 end
 
 function Enemy:draw()
-  love.graphics.setColor(150, 100, 200, 255)
+  if self.state == "stun" then
+    love.graphics.setColor(50, 50, 127, 255)
+  else
+    love.graphics.setColor(150, 100, 200, 255)
+  end
   love.graphics.rectangle("fill",
     self.x - self.width / 2,
     self.y - self.height / 2,
@@ -26,20 +33,32 @@ function Enemy:draw()
 end
 
 function Enemy:think(dt)
-  if rand:random() > 0.95 then
-    if rand:random() > 0.5 then
-      self.ax = self.moveForce / self.mass
-    else
-      self.ax = -self.moveForce / self.mass
+  if self.state == "stun" then
+    if (love.timer.getTime() - self.stunTimestamp) > 5 then
+      self.state = "normal"
+    end
+  else
+    if rand:random() > 0.95 then
+      if rand:random() > 0.5 then
+        self.ax = self.moveForce / self.mass
+      else
+        self.ax = -self.moveForce / self.mass
+      end
+    end
+    if rand:random() > 0.95 then
+      if rand:random() > 0.5 then
+        self.ay = self.moveForce / self.mass
+      else
+        self.ay = -self.moveForce / self.mass
+      end
     end
   end
-  if rand:random() > 0.95 then
-    if rand:random() > 0.5 then
-      self.ay = self.moveForce / self.mass
-    else
-      self.ay = -self.moveForce / self.mass
-    end
-  end
+end
+
+function Enemy:stun(dt)
+  self.state = "stun"
+  self.stunTimestamp = love.timer.getTime()
+  self:setAccel(0, 0)
 end
 
 function Enemy:drag(friction, dt)
